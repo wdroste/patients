1e7c026f63c75677195df2d4cfdd5a3b58450ecb
Will Droste <will@droste-usa.com>
2013-09-03 07:35:13 -0500

    Fixes to the resources

--- grails-app/conf/ScaffoldingResources.groovy
+++ grails-app/conf/ScaffoldingResources.groovy
@@ -2,7 +2,9 @@ modules = {
 	scaffolding {
 		dependsOn 'bootstrap'
 		resource url: 'css/scaffolding.css'
+        resource url: 'css/datepicker.css'
         resource url: 'css/bootstrap-duallistbox.css'
         resource url: 'js/bootstrap-duallistbox.js'
+        resource url: 'js/bootstrap-datepicker.js'
 	}
 }
